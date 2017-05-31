e3505521343ee27ebff4fe9947f0ad843264a571
Pedro G. Dias <pedro@digitaldias.com>
2017-05-31 10:54:43 +0200

    LuisExtension can now try to extract a PackageID from context
    This assumes that a LuisResult has been added to the conversationData

--- Dialogs/LuisExtensions.cs
+++ Dialogs/LuisExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Bot.Builder.Luis.Models;
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
 
 namespace LogisticBot.Dialogs
 {
@@ -7,6 +9,20 @@ namespace LogisticBot.Dialogs
         public static bool NotSureEnough(this LuisResult result)
         {
             return result.TopScoringIntent.Score <= 0.9;
+        }
+
+
+        public static bool TryToExtractPackageIdFromLuisResult(this IDialogContext context)
+        {
+            var luisResult = context.ConversationData.GetValue<LuisResult>("LuisResult");
+
+            EntityRecommendation packageIdEntity;
+            if (luisResult.TryFindEntity("PackageID", out packageIdEntity))
+            {
+                context.ConversationData.SetValue<string>("PackageID", packageIdEntity.Entity);
+                return true;
+            }
+            return false;
         }
     }
 }
