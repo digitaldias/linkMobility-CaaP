fa3af59df4a74db7c2a61af459f8320a0b200479
Pedro G. Dias <pedro@digitaldias.com>
2017-05-31 08:31:53 +0200

    Add generic base class for unit tests based on StructureMap.AutoMocking.Moq

---
+++ Link.CrossCutting/TestsFor.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using StructureMap.AutoMocking.Moq;
+
+namespace Link.CrossCutting
+{
+    public class TestsFor<TEntity> where TEntity : class
+    {
+        public TEntity Instance { get; set; }
+
+        public MoqAutoMocker<TEntity> AutoMock { get; set; }
+
+
+        public TestsFor()
+        {
+            AutoMock = new MoqAutoMocker<TEntity>();
+
+            Instance = AutoMock.ClassUnderTest;
+        }
+
+
+        public Mock GetMockFor<TContract>() where TContract : class
+        {
+            return Mock.Get(AutoMock.Get<TContract>());
+        }
+    }
+}
