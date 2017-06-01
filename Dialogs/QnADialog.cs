8500397448bca957018b02e9cf2b90433fde1911
Pedro G. Dias <pedro@digitaldias.com>
2017-06-01 13:11:22 +0200

    Add QnAMaker dialog example and crude IActivityLogger implementation
    #7

---
+++ Dialogs/QnADialog.cs
@@ -0,0 +1,38 @@
+﻿using Link.Domain.Contracts;
+using Microsoft.Bot.Builder.Dialogs;
+using QnAMakerDialog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LogisticBot.Dialogs
+{
+    public class QnADialog : QnAMakerDialog<object>
+    {
+        public QnADialog()
+        {
+            var settings = WebApiApplication.IoCResolver.GetInstance<ISettingsReader>();
+
+            base.SubscriptionKey = settings["QnaMaker.SubscriptionKey"];
+            base.KnowledgeBaseId = settings["QnaMaker.KnowledgeBaseId"];
+        }
+
+
+        public override async Task NoMatchHandler(IDialogContext context, string originalQueryText)
+        {
+            // TODO: Pass utterance to LUIS Dialog
+            await context.PostAsync($"Sorry, I couldn't find an answer for '{originalQueryText}'.");
+            context.Wait(MessageReceived);
+        }
+
+
+        [QnAMakerResponseHandler(90)]
+        public async Task LowScoreHandler(IDialogContext context, string originalQueryText, QnAMakerResult result)
+        {
+            await context.PostAsync($"I found an answer that might help...{result.Answer}.");
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ MyLogger.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Bot.Builder.History;
+using Microsoft.Bot.Connector;
+using System.Threading.Tasks;
+
+namespace LogisticBot
+{
+    public class MyLogger : IActivityLogger
+    {
+        public async Task LogAsync(IActivity activity)
+        {
+            // TODO: Implement this later
+            // Remember to register this instance in AutoFac config somewhere..:)
+            await Task.CompletedTask;
+        }
+    }
+}
