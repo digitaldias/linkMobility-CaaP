60aec2a5090b317a2f9ccc1ee2696b4c91f0ecf6
Pedro G. Dias <pedro@digitaldias.com>
2017-06-01 13:22:08 +0200

    Add example on how to use git to commit code.

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -18,7 +18,7 @@ namespace LogisticBot
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog()); 
             }
             else
             {
